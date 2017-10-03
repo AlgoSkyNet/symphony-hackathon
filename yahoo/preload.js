b9ac1ab3e018a5bc1befd453e818eb1eccfe966c
Thomas <tom@openfin.co>
2017-10-03 14:29:55 -0400

    index

--- yahoo/preload.js
+++ yahoo/preload.js
@@ -4,17 +4,19 @@ fin.desktop.InterApplicationBus.subscribe('*', 'context', (message) => {
 })
 
 document.addEventListener("DOMContentLoaded", event => {
-    // setInterval(()=> {
+    setInterval(()=> {
         let one = document.getElementById('mrt-node-Lead-0-FinanceHeader')
         let two = document.getElementById('mrt-node-Lead-1-FeatureBar')
         let three = document.getElementById('YDC-UH');
+        let four = document.getElementById('Nav-0-DesktopNav');
         const hide = el => {
             if(el & el.style.display !== 'none') {
                 el.style.display = 'none';
             }
         }
-        hide(one)
-        hide(two)
-        hide(three)
-    // },500);
+        hide(one);
+        hide(two);
+        hide(three);
+        hide(four);
+    },500);
 })
