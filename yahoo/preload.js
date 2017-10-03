b348d5e2f90eec1f56440e1263f77f4988ed71de
Thomas <tom@openfin.co>
2017-10-03 13:26:57 -0400

    changes

--- yahoo/preload.js
+++ yahoo/preload.js
@@ -1,9 +1,20 @@
 fin.desktop.InterApplicationBus.subscribe('*', 'context', (message) => {
     window.location.href = `https://finance.yahoo.com/quote/${message}`;
     
-    document.addEventListener("DOMContentLoaded", event => {
-        document.getElementById('mrt-node-Lead-0-FinanceHeader').style.display='none';
-        document.getElementById('mrt-node-Lead-1-FeatureBar').style.display='none';
-        document.getElementById('YDC-UH').style.display='none';
-    });
+})
+
+document.addEventListener("DOMContentLoaded", event => {
+    // setInterval(()=> {
+        let one = document.getElementById('mrt-node-Lead-0-FinanceHeader')
+        let two = document.getElementById('mrt-node-Lead-1-FeatureBar')
+        let three = document.getElementById('YDC-UH');
+        const hide = el => {
+            if(el & el.style.display !== 'none') {
+                el.style.display = 'none';
+            }
+        }
+        hide(one)
+        hide(two)
+        hide(three)
+    // },500);
 })
