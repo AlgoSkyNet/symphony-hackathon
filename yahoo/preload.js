2ca0767dbaf6058509494be179aeaba48ebe099b
Thomas O'Connor <tom@openfin.co>
2017-10-03 13:54:54 -0400

    Update preload.js

--- yahoo/preload.js
+++ yahoo/preload.js
@@ -2,8 +2,11 @@ fin.desktop.InterApplicationBus.subscribe('*', 'context', (message) => {
     window.location.href = `https://finance.yahoo.com/quote/${message}`;
     
 })
+console.log('outside docready')
 
 document.addEventListener("DOMContentLoaded", event => {
+    console.log('inside docready')
+
    // setInterval(()=> {
         let one = document.getElementById('mrt-node-Lead-0-FinanceHeader')
         let two = document.getElementById('mrt-node-Lead-1-FeatureBar')
@@ -13,6 +16,7 @@ document.addEventListener("DOMContentLoaded", event => {
                 el.style.display = 'none';
             }
         }
+        console.log('before first hide');
         hide(one)
         hide(two)
         hide(three)
